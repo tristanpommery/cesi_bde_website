2da3afce5bb6c1ef88f6945bf85cdde963231724
Etienne CARRE <etienne.carre@viacesi.fr>
2019-11-09 20:30:49 +0100

    dont know what i did but seems legit

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,93 +2,28 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
-use App\Form\ProductType;
-use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route ("/shop")
- */
 class ShopController extends AbstractController
 {
     /**
-     * @Route("/", name="shop", methods={"GET"})
+     * @Route("/shop", name="shop")
      */
-    public function index(ProductRepository $productRepository): Response
+    public function shop()
     {
-        return $this->render('main/product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+        return $this->render('shop/shop.html.twig', [
+            'controller_name' => 'ShopController',
         ]);
     }
 
     /**
-     * @Route("/new", name="product_new", methods={"GET","POST"})
+     * @Route("/shop/item/{id}", name="item")
      */
-    public function new(Request $request): Response
+    public function item($id)
     {
-        $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('product_index');
-        }
-
-        return $this->render('main/product/new.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
+        return $this->render('shop/item.html.twig', [
+            'id' => $id,
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="product_show", methods={"GET"})
-     */
-    public function show(Product $product): Response
-    {
-        return $this->render('main/product/show.html.twig', [
-            'product' => $product,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="product_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Product $product): Response
-    {
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('product_index');
-        }
-
-        return $this->render('main/product/edit.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="product_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Product $product): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($product);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('product_index');
     }
 }
