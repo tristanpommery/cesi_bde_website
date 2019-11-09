f3953ce70bd4cc7d1456d1ecd90a92df531ad085
Etienne CARRE <etienne.carre@viacesi.fr>
2019-11-09 20:39:32 +0100

    Last commit without sense, i promise

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/main/category")
+ * @Route("/category")
  */
 class CategoryController extends AbstractController
 {
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/main/event")
+ * @Route("/event")
  */
 class EventController extends AbstractController
 {
