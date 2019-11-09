13586d08d0e8abc3ad18e4f67d9d9b85a36c0e01
Tristan <tristan.pommery@viacesi.fr>
2019-11-09 19:23:00 +0100

    Admin Panel Creation & CRUD Additions

---
+++ src/Form/ProductType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('stock')
+            ->add('image')
+            ->add('category')
+            ->add('association')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
--- src/Migrations/Version20191109180408.php
+++ src/Migrations/Version20191109180408.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191109153238 extends AbstractMigration
+final class Version20191109180408 extends AbstractMigration
 {
     public function getDescription() : string
     {
