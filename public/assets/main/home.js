4e34a23df6440bd66e8faec6c8115988bf117c0c
Etienne CARRE <etienne.carre@viacesi.fr>
2019-11-12 13:50:59 +0100

    Added the display of the shop (index).

--- public/assets/main/home.js
+++ public/assets/main/home.js
@@ -7,7 +7,7 @@ document.addEventListener('wheel', function(event){
         document.getElementById("nav").style.top="560px"
         bannerText.style.fontSize="1em"
         banner.style.backgroundSize="2880px"
-    } else if(event.deltaY>0 ||(event.deltaY>0 && banner.style.height=="250px")){
+    } else if(event.deltaY>0 ||(event.deltaY<0 && window.scrollY>=102)){
         banner.style.height="200px"
         document.getElementById("nav").style.top="200px"
         bannerText.style.fontSize="0.5em"
