b93941f159b473c56e99fc196699b6e39ec5c574
Etienne CARRE <etienne.carre@viacesi.fr>
2019-11-14 03:49:26 +0100

    Hohoho

---
+++ public/assets/security/registration.js
@@ -0,0 +1,17 @@
+let productImage = document.getElementById('registration_form_image')
+let dragDropText = document.getElementById('dragDropText')
+
+productImage.addEventListener('dragenter', ()=>{
+    dragDropText.innerHTML='<i class="fas fa-file-import"></i>'
+    dragDropText.style.fontSize="4em"
+})
+
+productImage.addEventListener('dragleave', ()=>{
+    dragDropText.innerHTML='Glissez-déposez ici'
+    dragDropText.style.fontSize="1.5em"
+})
+
+productImage.addEventListener('drop', ()=>{
+    dragDropText.innerHTML='<i class="far fa-file-image"></i>'
+    dragDropText.style.fontSize="4em"
+})
