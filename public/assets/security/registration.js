4df41231da7896633198dc35cbad43af2a0166a8
Etienne CARRE <etienne.carre@viacesi.fr>
2019-11-14 22:57:47 +0100

    Allez vieeeeeens c'est bientôt la fiiiiiinnnnnn

--- public/assets/security/registration.js
+++ public/assets/security/registration.js
@@ -1,5 +1,8 @@
 let productImage = document.getElementById('registration_form_image')
 let dragDropText = document.getElementById('dragDropText')
+let terms = document.getElementById('termsCheckbox')
+let checked = document.getElementById('checked')
+let notChecked = document.getElementById('notChecked')
 
 productImage.addEventListener('dragenter', ()=>{
     dragDropText.innerHTML='<i class="fas fa-file-import"></i>'
@@ -15,3 +18,13 @@ productImage.addEventListener('drop', ()=>{
     dragDropText.innerHTML='<i class="far fa-file-image"></i>'
     dragDropText.style.fontSize="4em"
 })
+
+terms.addEventListener( 'change', function() {
+    if(this.checked) {
+        checked.style.display="block"
+        notChecked.style.display="none"
+    } else {
+        checked.style.display="none"
+        notChecked.style.display="block"
+    }
+});
