5d64380f94e82303b398c708007f41feaa425307
Tristan <tristan.pommery@viacesi.fr>
2019-11-15 02:32:47 +0100

    PUTAIN GIT NIQUE TES MORTS

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -1,16 +1,20 @@
 <?php
-
 namespace App\Service\Cart;
 
-use App\Entity\User;
-use Twig\Environment;
 use App\Entity\Product;
+use App\Entity\User;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Twig\Environment;
 
 class CartService
 {
+
     protected $session;
     protected $productRepository;
 
@@ -20,49 +24,66 @@ class CartService
         $this->productRepository = $productRepository;
     }
 
-    public function add(int $id)
+    public function add(int $id, Request $request)
     {
+
+        $value = $request->cookies->get('cartCookie');
         $cart = $this->session->get('cart', []);
+
+        if ($value !== null) {
+            $cart = json_decode($value, true);
+        }
+
         if (!empty($cart[$id])) {
             $cart[$id]++;
         } else {
             $cart[$id] = 1;
         }
+        $cookie = new Cookie('cartCookie', json_encode($cart), (time() + 7 * 24 * 60 * 60));
         $this->session->set('cart', $cart);
+        $res = new Response();
+        $res->headers->setCookie($cookie);
+        $res->send();
     }
 
-    public function remove(int $id)
+    public function remove(int $id, Request $request)
     {
         $cart = $this->session->get('cart', []);
-
-        if (empty($cart[$id]) or $cart[$id] === 1) {
-            $this->delete($id);
-        } else {
-            $cart[$id]--;
-            $this->session->set('cart', $cart);
+        $value = $request->cookies->get('cartCookie');
+        if ($value !== null) {
+            $cart = json_decode($value, true);
         }
-    }
-
-    public function delete(int $id)
-    {
-        $cart = $this->session->get('cart', []);
         if (!empty($cart[$id])) {
-            unset($cart[$id]);
+            $cart[$id]--;
+            if ($cart[$id] == 0) {
+                unset($cart[$id]);
+            }
         }
+
+        $cookie = new Cookie('cartCookie', json_encode($cart), time() + (7 * 24 * 60 * 60));
         $this->session->set('cart', $cart);
+        $res = new Response();
+        $res->headers->setCookie($cookie);
+        $res->send();
     }
 
     public function getFullCart(): array
     {
-        $cart = $this->session->get('cart', []);
-        $cartWithData = [];
-        foreach ($cart as $id => $quantity) {
-            $cartWithData[] = [
-                'product' => $this->productRepository->find($id),
-                'quantity' => $quantity
-            ];
+        if (isset($_COOKIE['cartCookie'])) {
+            if ($_COOKIE['cartCookie'] !== null) {
+                $cart = json_decode($_COOKIE['cartCookie']);
+                $cartWithData = [];
+                foreach ($cart as $id => $quantity) {
+
+                    $cartWithData[] = [
+                        'product' => $this->productRepository->find($id),
+                        'quantity' => $quantity
+                    ];
+                }
+                return $cartWithData;
+            }
         }
-        return $cartWithData;
+        return $cartWithData = [];
     }
 
     public function getTotal(): float
@@ -88,11 +109,11 @@ class CartService
         return false;
     }
 
-    public function checkout(\Swift_Mailer $mailer, Environment $renderer, EntityManagerInterface $em)
+    public function checkout(\Swift_Mailer $mailer, Environment $renderer, EntityManagerInterface $em, UserInterface $user)
     {
         $members = $em->getRepository(User::class)->findByRole('ROLE_BDE');
         if ($members) {
-            foreach ($members as $member) {
+            foreach ($members as $member){
 
 
                 $message = (new \Swift_Message('Récapitulatif commande'))
@@ -106,9 +127,11 @@ class CartService
                         'text/html'
                     );
                 $mailer->send($message);
+
             }
         }
         unset($_COOKIE['cartCookie']);
         setcookie('cartCookie', null, -1, '/');
     }
 }
+
