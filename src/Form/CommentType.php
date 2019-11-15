2dbcff879ea8caeb6311f57a1fabba68206d6b69
Tristan <tristan.pommery@viacesi.fr>
2019-11-15 04:43:10 +0100

    Comment Addition for images

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,10 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Event;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,16 +14,6 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('content', TextareaType::class)
-            ->add('createdAt', DateType::class)
-            ->add('event', EntityType::class, [
-                'class'=>Event::class,
-                'choice_label'=>'name'
-            ])
-            ->add('product', EntityType::class, [
-                'class'=>Event::class,
-                'choice_label'=>'name'
-            ])
-            ->add('fakeUser')
         ;
     }
 
